cbc25a846c5f4293b6ba9606f38c89cb1d1fbf5f
lucascn21 <lucascn21@gmail.com>
2023-06-30 16:31:30 -0300

    Table dynamically disables, needs further testing

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,68 +1,97 @@
 import React from "react";
 import "./BasicTable.css";
-let sectors = [...Array(7).keys()];
-let binomials = [...Array(7).keys()];
-let maxTimeForGroup = "6:00";
-console.dir(sectors);
-console.dir(binomials);
-export const BasicTable = () => {
-  function hhmmToDecimal(hourHHMM = "00:00") {
-    console.dir(hourHHMM);
+import { Binomial } from "../models/binomial";
+//Use a folder or structure this better
+function hhmmToDecimal(hourHHMM = "00:00") {
+  //console.dir(hourHHMM);
 
-    //console.dir(typeof hourHHMM);
-    let timeArray = hourHHMM.split(":");
-    // console.dir(timeArray.length === 2);
-    if (timeArray[0] == "00") timeArray[0] = "12";
-    return parseFloat(
-      parseInt(timeArray[0], 10) + parseInt(timeArray[1], 10) / 60
-    );
-  }
-  function decimalToHHMM(decimalHour) {
-    let hrs = parseInt(Number(decimalHour));
-    let min = Math.round((Number(decimalHour) - hrs) * 60);
-    console.dir(min);
-    //console.dir(min.toString().length);
-    return `${hrs}:${
-      min.toString().length < 2 ? min.toString().padStart(2, "0") : min
-    }`;
-  }
+  //console.dir(typeof hourHHMM);
+  let timeArray = hourHHMM.split(":");
+  // console.dir(timeArray.length === 2);
+  if (timeArray[0] === "00") timeArray[0] = "12";
+  return parseFloat(
+    parseInt(timeArray[0], 10) + parseInt(timeArray[1], 10) / 60
+  );
+}
+function decimalToHHMM(decimalHour) {
+  let hrs = parseInt(Number(decimalHour));
+  let min = Math.round((Number(decimalHour) - hrs) * 60);
+  //console.dir(min);
+  //console.dir(min.toString().length);
+  return `${hrs}:${
+    min.toString().length < 2 ? min.toString().padStart(2, "0") : min
+  }`;
+}
+
+//Still deciding how to handle constants and models
+let sectors = [...Array(7).keys()];
+let maxTimeForGroup = "06:00";
+let binomials = [];
+for (let index = 0; index < 7; index++) {
+  binomials.push(
+    //Will probably handle strings so hhmmtodecimal stays for now
+    new Binomial(index, ["asd"], [], hhmmToDecimal(maxTimeForGroup))
+  );
+}
+
+export const BasicTable = () => {
   const handleChange = (e) => {
-    //console.dir(e.target);
-    //console.dir(e.target.parentElement);
-    //console.dir(e.target);
     let amTime = undefined,
       pmTime = undefined;
-    if (e.target.parentElement.children[0].value) {
-      console.dir("am time changed");
-      amTime = 13 - hhmmToDecimal(e.target.parentElement.children[0].value);
-    }
-    if (e.target.parentElement.children[1].value) {
-      console.dir("pm time changed");
-      pmTime = hhmmToDecimal(e.target.parentElement.children[1].value) ;
-    }
-    let potentialPatrolTime = amTime + pmTime;
-    console.dir(
-      `amTime: ${amTime} || pmTime: ${pmTime} || maxTimeForGroup: ${maxTimeForGroup} || || ${decimalToHHMM(
-        potentialPatrolTime
-      )} || ${hhmmToDecimal(maxTimeForGroup)}`
-    );
+    let amInput = e.target.parentElement.children[0];
+    let pmInput = e.target.parentElement.children[1];
+    let potentialPatrolTime = 0;
 
-    //console.dir(hhmmToDecimal(e.target.parentElement.children[0].value));
-    //console.dir(hhmmToDecimal(e.target.parentElement.children[1].value));
-    /*
-    let amTime = decimalToHHMM(
-      hhmmToDecimal(e.target.parentElement.children[0].value)
-    );
+    if (amInput.value && pmInput.value) {
+      let { binomial, sector } = e.target.dataset;
+      //12 to 24 hours format shenanigans
+      amTime = 13 - hhmmToDecimal(amInput.value);
+      pmTime = hhmmToDecimal(pmInput.value) - 13;
+      potentialPatrolTime = amTime + pmTime;
 
-    let pmTime = decimalToHHMM(
-      hhmmToDecimal(e.target.parentElement.children[1].value)
-    );
-    */
-    //console.dir(amTime);
-    //console.dir(pmTime);
-    //console.dir(`${e.target.value} ${e.target.dataset.meridiem}`);
-    //console.dir(e.target.dataset);
+      //This could be cleaner, must think how to handle error / warning messages/toasts so it stays like this for now
+      binomials[binomial].patrolTime[sector] = potentialPatrolTime;
+      if (binomials[binomial].getUnallottedHours() < 0) {
+        /*
+        alert(
+          `binomial ${binomial} in sector ${sector} Expected patrol time: ${binomialTest.getTotalHoursInPatrol()} exceeds ${hhmmToDecimal(
+            maxTimeForGroup
+          )} which is the max patrol time per group`
+        );
+        */
+        amInput.setCustomValidity("wtf richard");
+        pmInput.setCustomValidity("wtf richard");
+      } else {
+        amInput.setCustomValidity("");
+        pmInput.setCustomValidity("");
+        //alert(`binomial ok. Remaining ${binomialTest.getUnallottedHours()}`);
+      }
+
+      //Make a function that handles this so its cleaner
+      const elements = document.querySelectorAll(
+        `[data-binomial="${binomials[binomial].binomialDataId}"]`
+      );
+      if (binomials[binomial].getUnallottedHours() === 0) {
+        for (const binomialRow of elements) {
+          !binomialRow.value && binomialRow.setAttribute("disabled", true);
+        }
+      } else {
+        for (const binomialRow of elements) {
+          !binomialRow.value && binomialRow.removeAttribute("disabled");
+        }
+      }
+      console.dir(
+        `amTime: ${amTime} || pmTime: ${pmTime} || maxTimeForGroup: ${maxTimeForGroup} ||getUnallottedHours ${binomials[
+          binomial
+        ].getUnallottedHours()} || getTotalHoursInPatrol ${binomials[
+          binomial
+        ].getTotalHoursInPatrol()} || Remaining ${binomials[
+          binomial
+        ].getUnallottedHours()}`
+      );
+    }
   };
+
   return (
     <>
       <table>
@@ -77,14 +106,16 @@ export const BasicTable = () => {
         <tbody>
           {binomials.map((binomial) => {
             return (
-              <tr key={binomial + 1}>
-                <th>Group {binomial + 1}</th>
+              <tr key={binomial.binomialDataId + 1}>
+                <th>Group {binomial.binomialDataId + 1}</th>
                 {sectors.map((sector) => {
                   return (
-                    <td key={`${binomial + 1}${sector + 1}`}>
+                    <td key={`${binomial.binomialDataId + 1}${sector + 1}`}>
                       <input
-                        id={`PatrolStart-B${binomial + 1}-S${sector + 1}`}
-                        data-binomial={binomial + 1}
+                        id={`PatrolStart-B${binomial.binomialDataId + 1}-S${
+                          sector + 1
+                        }`}
+                        data-binomial={binomial.binomialDataId + 1}
                         data-sector={sector + 1}
                         data-meridiem={"AM"}
                         type="time"
@@ -93,17 +124,19 @@ export const BasicTable = () => {
                         max="11:59"
                         pattern="[0-2][0-9]:[0-5][0-9]"
                         onKeyDown={(e) => {
-                          console.dir(e);
-                          console.dir(e.key === "Enter");
+                          // console.dir(e);
+                          //  console.dir(e.key === "Enter");
                         }}
                         onInput={(e) => {
-                          console.dir(e);
+                          //console.dir(e);
                         }}
                         required
                       />
                       <input
-                        id={`PatrolEnd-B${binomial + 1}-S${sector + 1}`}
-                        data-binomial={binomial + 1}
+                        id={`PatrolEnd-B${binomial.binomialDataId + 1}-S${
+                          sector + 1
+                        }`}
+                        data-binomial={binomial.binomialDataId + 1}
                         data-sector={sector + 1}
                         data-meridiem={"PM"}
                         type="time"
@@ -112,8 +145,8 @@ export const BasicTable = () => {
                         max="23:59"
                         pattern="[0-2][0-9]:[0-5][0-9]"
                         onKeyDown={(e) => {
-                          console.dir(e);
-                          console.dir(e.key === "Enter");
+                          //console.dir(e);
+                          //console.dir(e.key === "Enter");
                         }}
                         required
                       />
